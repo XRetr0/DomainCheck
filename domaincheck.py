95e25eee310c6d367dfa61edd2c6efffb874a329
XRetr0 <kolesnic284@gmail.com>
2020-03-13 16:20:30 +0200

    It works, but slowly

--- domaincheck.py
+++ domaincheck.py
@@ -1,22 +1,52 @@
 import requests
 import click
+import os
 
 domains = []
 def check(path):
     global domains
     with open(path, 'r') as f:
         old_data = f.read()
-
     new_data = old_data.replace('<BR>', '\n')
-
     with open(path,'w') as f:
         f.write(new_data)
-
     with open(path, 'r') as f:
         domains = f.readlines()
 
+    if '/' in path:
+        file = str(path.split('/')[-1])
+        file = file.replace('.txt','')
+        print("Your file: " + file)
+        try:
+            # Create target Directory
+            os.mkdir(file)
+        except FileExistsError:
+            print("Error, this folder already exists")
+            inp = str(input("Clear result files? Y/n: "))
+            inp = inp.lower()
+            if inp == "y":
+                with open(file + '/' + 'result_not200.txt', 'w') as f:
+                    f.write("")
+
     for d in domains:
-        print(d)
+        d = d.replace('\n','')
+        if 'http:' in d or 'https:' in d:
+            pass
+        else:
+            d = "http://"+d
+        try:
+            req = requests.get(d,timeout=0.6)
+            st = req.status_code
+            print ("Url: "+d+" Status code: "+str(st))
+            if st == 301 or st == 302 or st == 403 or st == 404:
+                with open(file + '/'+'result_not200.txt', 'a') as f:
+                    f.write(d + '\n')
+            elif st == 200:
+                with open(file + '/'+'result_200.txt', 'a') as f:
+                    f.write(d + '\n')
+        except:
+            pass
+
 
 
 @click.command()
