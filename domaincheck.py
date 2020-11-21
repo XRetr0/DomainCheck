e21707dd209adb198a12dfcb430df47ff6fea24c
XmlEntity <dqqsaddasa@whzsa.com>
2020-11-21 15:26:17 +0200

    https support and some changes

--- domaincheck.py
+++ domaincheck.py
@@ -1,52 +1,69 @@
 import requests
 import click
 import os
+import urllib3
 
 domains = []
+folder_name = ''
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def dir_create(path):
+    global folder_name
+    if '/' in path:
+        folder_name = str(path.split('/')[-1]).replace('.txt', '')
+        print("Result will be saved in " + folder_name)
+    else:
+        folder_name = path.replace('.txt', '')
+
+    try:
+        os.mkdir(folder_name)  # Create target Directory
+    except FileExistsError:
+        print("Error, this folder already exists")
+        inp = str(input("Clear result files? Y/n: "))
+        inp = inp.lower()
+        if inp == "y":
+            with open(folder_name + '/' + 'result_not200.txt', 'w') as f:
+                f.write("")
+
+
 def check(path):
     global domains
     with open(path, 'r') as f:
         old_data = f.read()
     new_data = old_data.replace('<BR>', '\n')
-    with open(path,'w') as f:
+    with open(path, 'w') as f:
         f.write(new_data)
     with open(path, 'r') as f:
         domains = f.readlines()
 
-    if '/' in path:
-        file = str(path.split('/')[-1])
-        file = file.replace('.txt','')
-        print("Result will be saved in " + file)
-        try:
-            # Create target Directory
-            os.mkdir(file)
-        except FileExistsError:
-            print("Error, this folder already exists")
-            inp = str(input("Clear result files? Y/n: "))
-            inp = inp.lower()
-            if inp == "y":
-                with open(file + '/' + 'result_not200.txt', 'w') as f:
-                    f.write("")
+    dir_create(path)
 
     for d in domains:
-        d = d.replace('\n','')
+        d = d.replace('\n', '')
         if 'http:' in d or 'https:' in d:
             pass
         else:
-            d = "http://"+d
-        try:
-            req = requests.get(d,timeout=0.6)
-            st = req.status_code
-            print ("Url: "+d+" Status code: "+str(st))
-            if st == 301 or st == 302 or st == 403 or st == 404 or st == 500:
-                with open(file + '/'+'result_not200.txt', 'a') as f:
-                    f.write(d + '\n')
-            elif st == 200:
-                with open(file + '/'+'result_200.txt', 'a') as f:
-                    f.write(d + '\n')
-        except:
-            pass
+            d = "http://" + d
 
+        try:
+            req = requests.get(d, timeout=3, allow_redirects=True, verify=False)
+            st = req.status_code
+            if 'https://' in req.url:
+                d = req.url
+            print("Url: " + d + " Status code: " + str(st))
+            if st == 200:  # bad code
+                with open(folder_name + '/'+'result_200.txt', 'a') as f:
+                    f.write(d + '\n')
+            else:
+                with open(folder_name + '/'+'result_not200.txt', 'a') as f:
+                    f.write(d + '\n')
+        except requests.exceptions.SSLError as ex_ssl:
+            print(str(ex_ssl))
+            print("SSL ERROR!")
+
+        except requests.exceptions.ConnectionError as ex:
+            pass
 
 
 @click.command()
@@ -64,6 +81,7 @@ def main(path):
     """
 
     check(path)
+
 
 if __name__ == "__main__":
     main()
