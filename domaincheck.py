c0210fee2c4bbd93d40cd4a3afdbd13865b6c12b
XmlEntity <dqqsaddasa@whzsa.com>
2020-11-22 22:56:44 +0200

    added redirects and original url to log

--- domaincheck.py
+++ domaincheck.py
@@ -5,19 +5,19 @@ import urllib3
 
 domains = []
 folder_name = ''
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)  # Disable https warnings
 
 
 def dir_create(path):
     global folder_name
     if '/' in path:
-        folder_name = str(path.split('/')[-1]).replace('.txt', '')
+        folder_name = str(path.split('/')[-1]).replace('.txt', '')  # Get folder name from the path
         print("Result will be saved in " + folder_name)
     else:
         folder_name = path.replace('.txt', '')
 
     try:
-        os.mkdir(folder_name)  # Create target Directory
+        os.mkdir(folder_name)
     except FileExistsError:
         print("Error, this folder already exists")
         inp = str(input("Clear result files? Y/n: "))
@@ -41,25 +41,24 @@ def check(path):
 
     for d in domains:
         d = d.replace('\n', '')
-        if 'http:' in d or 'https:' in d:
+        if 'http://' in d or 'https://' in d:
             pass
         else:
             d = "http://" + d
 
         try:
             req = requests.get(d, timeout=3, allow_redirects=True, verify=False)
-            st = req.status_code
+            d_redirected = ''
             if 'https://' in req.url:
-                d = req.url
-            print("Url: " + d + " Status code: " + str(st))
-            if st == 200:  # bad code
+                d_redirected = ' --> ' + req.url
+            print("Url: " + d + d_redirected + " Status code: " + str(req.status_code))
+            if req.status_code == 200:
                 with open(folder_name + '/'+'result_200.txt', 'a') as f:
-                    f.write(d + '\n')
+                    f.write(d + d_redirected + '\n')
             else:
                 with open(folder_name + '/'+'result_not200.txt', 'a') as f:
-                    f.write(d + '\n')
+                    f.write(d + d_redirected + '\n')
         except requests.exceptions.SSLError as ex_ssl:
-            print(str(ex_ssl))
             print("SSL ERROR!")
 
         except requests.exceptions.ConnectionError as ex:
